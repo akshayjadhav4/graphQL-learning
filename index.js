ebaee890da1c22c0b0a9a19cbc0d8e94ebb0ed2e
AKSHAY <akshayjadhav461999@gmail.com>
2020-12-15 15:43:57 +0530

    graphQL setup done

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import resolvers from "./resolvers";
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Set up Done.");
 });
+
+const root = resolvers;
+
+app.use("/graphql", graphqlHTTP({ schema, rootValue: root, graphiql: true }));
 
 const PORT = 2004;
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
